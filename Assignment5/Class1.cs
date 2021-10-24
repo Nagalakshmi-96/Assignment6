f804c0e0e54eda2cffe336f6e955db7c3d073929
Nagalakshmi-96 <93072593+Nagalakshmi-96@users.noreply.github.com>
2021-10-24 21:46:54 +0530

    changes

---
+++ Assignment5/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Configuration;
+
+class Class1
+{
+    static void Main()
+    {
+        string ScheduledTime = System.Configuration.ConfigurationManager.AppSettings["ScheduledTime"];
+        string[] ScheduledTimeParts = ScheduledTime.Split(new char[1] { ':' });
+
+        Console.WriteLine("The Scheduled time for the Function --> {0}", ScheduledTime);
+
+        while (true)
+        {
+            DateTime currentDate = DateTime.Now;
+
+            DateTime ScheduledDateTime = new DateTime(currentDate.Year, currentDate.Month, currentDate.Day, int.Parse(ScheduledTimeParts[0]), int.Parse(ScheduledTimeParts[1]), int.Parse(ScheduledTimeParts[2]));
+
+            TimeSpan ts;
+
+            if (ScheduledDateTime > currentDate)
+                ts = ScheduledDateTime - currentDate;
+            else
+            {
+                ScheduledDateTime = ScheduledDateTime.AddDays(1);
+                ts = ScheduledDateTime - currentDate;
+            }
+
+            Task.Delay(ts).Wait();
+            DisplayMessage();
+
+
+        }
+        Console.Read();
+    }
+
+    static void DisplayMessage()
+    {
+        Console.WriteLine("***********Method is called at {0}***********",DateTime.Now);
+    }
+}
+
---
+++ Assignment5/Class2.cs
@@ -0,0 +1,32 @@
+﻿//using System;
+//using System.Threading;
+//using System.Threading.Tasks;
+
+
+
+////Sample Solution Using Timer
+
+
+
+
+
+
+//class Class2
+//{
+//    private static Timer timer;
+
+//    static void Main(string[] args)
+//    {
+//        timer = new Timer(callback: new TimerCallback(TimerTask),state: null,dueTime: 0,period: 86400000);
+//        while (true)
+//        {}
+        
+//    }
+
+//    private static void TimerTask(object timerState)
+//    {
+//        Console.WriteLine("Function Called at {0}",DateTime.Now);
+//    }
+
+
+//}
